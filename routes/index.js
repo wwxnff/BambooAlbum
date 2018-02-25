36a5c44020cb65cbc5b4cea2b9c41a50f83950d4
Ruihan Wang <harrywangshc@gmail.com>
2018-02-25 09:04:06 -0500

    update message

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var multer = require('multer');
 var upload = multer(); 
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
 
 router.use(bodyParser.json());
@@ -16,8 +17,8 @@ router.use(session({secret: "Bamboo Album"}));
 var connectionString = "mongodb+srv://admin:admin@test-zokjp.mongodb.net/test";
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public','index.html'));
 });
 
 
@@ -26,17 +27,17 @@ router.post('/register', function(req, res, next) {
 	  var dbo = client.db("users");
 	  if (!req.body.userId || !req.body.password){
 	  res.status("400");
-	  res.send("Please fill userid or password");	
+			 res.send({info:"error"});	
 	  }else{
 	  	var existed = false;
 		dbo.collection("usersData").findOne({userId : req.body.userId}, function(err,result){
 			if (err) throw err;
 			if (result !== null){
-			res.send("The username already existed");
+			res.send({info:"error"});
 			}else{
 				dbo.collection("usersData").insertOne(req.body,function(err,resp){
 					if (err) throw err;
-					else res.send("Success");
+			 res.send({info:"success"});
 				});
 		}
 		});
@@ -46,22 +47,23 @@ router.post('/register', function(req, res, next) {
 
 router.post('/login',function(req,res){
 	if (req.session.user !== null){
-		res.send("Error");
-	}else{
+ res.send({info:"error"});
+ 	}else{
 MongoClient.connect(connectionString,function(err,client){
 	var dbo = client.db("users");
 	if (!req.body.userId || !req.body.password){
 	  res.status("400");
-	  res.send("Please fill userid or password");	
+	  //res.send("Please fill userid or password");	
+	  res.send({info:"error"});
 	}else{
 		dbo.collection("usersData").findOne({userId : req.body.userId},function(err,result){
 			if (result === null) res.send("The user doesn't exist");
 			else{
 				if (result.password === req.body.password){
 					req.session.user = req.body.userId;
-					res.send("You are" + req.session.user);
+					 res.send({info:"error"});
 				}else{
-					res.send("incorrect password, enter it again");
+			 res.send({info:"success"});
 				}
 			}
 		});
